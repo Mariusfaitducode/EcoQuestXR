bb04606eddc2c9ca4a31aba10715c5dea4e79612
Victor Goudal <vi.goudal@gmail.com>
2024-04-28 21:57:05 +0200

    working version of scale / rotate / translate

--- Assets/Scripts/Controller/MapController.cs
+++ Assets/Scripts/Controller/MapController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,18 +12,31 @@ public class MapController : MonoBehaviour
     
     // Scale
     private float _scalingAxis = 0.0f;
-    public GameObject mapGenerator;
-    public float scalingSpeed = 1f;
+    //public GameObject mapGenerator;
+    public float scalingSpeed = 0.001f;
     
     // Move
     private Vector3 _initialPosition;
     private Vector2 _tempPosition;
     private bool _initialPositionFound = false;
     private Vector2 _movingAxis = Vector2.zero;
-    public float movingSpeed = 1f;
+    public float movingSpeed = 0.05f;
 
     // Update is called once per frame
     void Update()
+    {
+        /*Vector2 leftAxisTemp = OVRInput.Get(OVRInput.RawAxis2D.LThumbstick);
+        Vector2 rightAxisTemp = OVRInput.Get(OVRInput.RawAxis2D.RThumbstick);
+
+        Debug.Log("leftinput : " + OVRInput.Get(OVRInput.RawAxis2D.LThumbstick)+ "rightinput : " + OVRInput.Get(OVRInput.RawAxis2D.RThumbstick)+ "varleft : " +leftAxisTemp+ "varright : "+rightAxisTemp);*/
+        GetInitialPosition();
+
+        ScaleRotate();
+        
+        Move();
+    }
+
+    void GetInitialPosition()
     {
         if (!_initialPositionFound)
         {
@@ -33,40 +47,48 @@ public class MapController : MonoBehaviour
                 Debug.Log("Initial position found : " + _initialPosition);
             }
         }
-        
-        // Get right controller thumbstick axis
-        Vector2 rightAxisTemp = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
-        // Get right controller thumbstick axis
-        bool rightButtonThumbstickTemp = OVRInput.Get(OVRInput.Button.SecondaryThumbstick);
-        // Get left controller thumbstick axis
-        Vector2 leftAxisTemp = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+    }
 
-        // Rotate
-        // TODO : Lock an axis 
-        if (_rotationAxis != leftAxisTemp.x)
-        {
-            _rotationAxis = leftAxisTemp.x;
-            float newY = leftAxisTemp.x * rotationSpeed;
-            transform.Rotate(0.0f, newY, 0.0f, Space.World);
-            Debug.Log("Rotating : " + transform.rotation);
-        }
+    void ScaleRotate()
+    {
+        // Get left controller thumbstick axis
+        Vector2 leftAxisTemp = OVRInput.Get(OVRInput.RawAxis2D.LThumbstick);
         
-        // Scale
-        if (_scalingAxis != leftAxisTemp.y)
+        // Rotate or Scale
+        if (Math.Abs(leftAxisTemp.x) > 0.5 | Math.Abs(leftAxisTemp.y) > 0.5)
         {
-            _scalingAxis = leftAxisTemp.y;
-            float newS = mapGenerator.transform.localScale.x + leftAxisTemp.y * rotationSpeed;
-            mapGenerator.transform.localScale = new Vector3(newS, newS, newS);
-            Debug.Log("Scaling : " + transform.rotation);
+            // Check iif rotating rather than scaling
+            if (Math.Abs(leftAxisTemp.x) > Math.Abs(leftAxisTemp.y))
+            {
+                _rotationAxis = leftAxisTemp.x;
+                float newY = leftAxisTemp.x * rotationSpeed * Time.deltaTime;
+                transform.Rotate(0.0f, newY, 0.0f, Space.World);
+                Debug.Log("Rotating : " + transform.rotation);
+            }
+            else
+            {
+                _scalingAxis = leftAxisTemp.y;
+                float newS = transform.localScale.x + leftAxisTemp.y * scalingSpeed * Time.deltaTime;
+                transform.localScale = new Vector3(newS, newS/10, newS);
+                Debug.Log("Scaling : " + transform.localScale);
+            }
         }
+    }
+
+    void Move()
+    {
+        // Get right controller thumbstick axis
+        Vector2 rightAxisTemp = OVRInput.Get(OVRInput.RawAxis2D.RThumbstick);
+        // Get right controller thumbstick axis
+        bool rightButtonThumbstickTemp = OVRInput.Get(OVRInput.RawButton.RThumbstick);
         
         // moving
         // TODO : move dans la direction que pointe le joueur
         if (_movingAxis != rightAxisTemp)
         {
             _movingAxis = rightAxisTemp;
-            _tempPosition = new Vector2(_tempPosition.x + rightAxisTemp.x * movingSpeed,
-                _tempPosition.y + rightAxisTemp.y * movingSpeed);
+            _tempPosition = new Vector2(_tempPosition.x + rightAxisTemp.x * movingSpeed * Time.deltaTime,
+                _tempPosition.y + rightAxisTemp.y * movingSpeed * Time.deltaTime);
             
             transform.position = new Vector3(_initialPosition.x + _tempPosition.x, _initialPosition.y, _initialPosition.z + _tempPosition.y);
             Debug.Log("Moving : " + transform.rotation);
