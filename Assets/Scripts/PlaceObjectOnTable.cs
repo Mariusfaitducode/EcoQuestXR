65d3ed6695879387b1e72f4529aaacbb581d6243
Victor Goudal <vi.goudal@gmail.com>
2024-04-28 16:33:31 +0200

    OVR update + add PlaceObjectOnTable

---
+++ Assets/Scripts/PlaceObjectOnTable.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceObjectOnTable : MonoBehaviour
+{
+    private GameObject _table;
+    private bool _haveTable = false;
+
+    void Update()
+    {
+        if (!_haveTable)
+        {
+            _table = GameObject.FindGameObjectWithTag("Table");
+
+            if (_table != null)
+            {
+                _haveTable = true;
+                Vector3 tablePosition = _table.transform.position;
+                transform.rotation = Quaternion.identity;
+                transform.position = new Vector3(tablePosition.x, tablePosition.y, tablePosition.z);
+                Debug.Log("Table found");
+            }
+        }
+        
+    }
+
+   
+}
