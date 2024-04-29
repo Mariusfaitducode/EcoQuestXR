1f93985e2fe2c301356c9089420298321b983355
Victor Goudal <vi.goudal@gmail.com>
2024-04-29 09:59:35 +0200

    working moving map

--- Assets/Scripts/Controller/MapController.cs
+++ Assets/Scripts/Controller/MapController.cs
@@ -12,7 +12,7 @@ public class MapController : MonoBehaviour
     
     // Scale
     private float _scalingAxis = 0.0f;
-    //public GameObject mapGenerator;
+    public GameObject mapGenerator;
     public float scalingSpeed = 0.001f;
     
     // Move
@@ -21,14 +21,11 @@ public class MapController : MonoBehaviour
     private bool _initialPositionFound = false;
     private Vector2 _movingAxis = Vector2.zero;
     public float movingSpeed = 0.05f;
+    public GameObject player;
 
     // Update is called once per frame
     void Update()
     {
-        /*Vector2 leftAxisTemp = OVRInput.Get(OVRInput.RawAxis2D.LThumbstick);
-        Vector2 rightAxisTemp = OVRInput.Get(OVRInput.RawAxis2D.RThumbstick);
-
-        Debug.Log("leftinput : " + OVRInput.Get(OVRInput.RawAxis2D.LThumbstick)+ "rightinput : " + OVRInput.Get(OVRInput.RawAxis2D.RThumbstick)+ "varleft : " +leftAxisTemp+ "varright : "+rightAxisTemp);*/
         GetInitialPosition();
 
         ScaleRotate();
@@ -57,20 +54,23 @@ public class MapController : MonoBehaviour
         // Rotate or Scale
         if (Math.Abs(leftAxisTemp.x) > 0.5 | Math.Abs(leftAxisTemp.y) > 0.5)
         {
-            // Check iif rotating rather than scaling
+            // Check if rotating rather than scaling
             if (Math.Abs(leftAxisTemp.x) > Math.Abs(leftAxisTemp.y))
             {
-                _rotationAxis = leftAxisTemp.x;
                 float newY = leftAxisTemp.x * rotationSpeed * Time.deltaTime;
                 transform.Rotate(0.0f, newY, 0.0f, Space.World);
                 Debug.Log("Rotating : " + transform.rotation);
             }
             else
             {
-                _scalingAxis = leftAxisTemp.y;
-                float newS = transform.localScale.x + leftAxisTemp.y * scalingSpeed * Time.deltaTime;
+                // Cube
+                /*float newS = transform.localScale.x + leftAxisTemp.y * scalingSpeed * Time.deltaTime;
                 transform.localScale = new Vector3(newS, newS/10, newS);
-                Debug.Log("Scaling : " + transform.localScale);
+                Debug.Log("Scaling : " + transform.localScale);*/
+                // Map
+                float newS = mapGenerator.transform.localScale.x + leftAxisTemp.y * scalingSpeed * Time.deltaTime;
+                mapGenerator.transform.localScale = new Vector3(newS, newS/10, newS);
+                Debug.Log("Scaling : " + mapGenerator.transform.localScale);
             }
         }
     }
@@ -81,19 +81,26 @@ public class MapController : MonoBehaviour
         Vector2 rightAxisTemp = OVRInput.Get(OVRInput.RawAxis2D.RThumbstick);
         // Get right controller thumbstick axis
         bool rightButtonThumbstickTemp = OVRInput.Get(OVRInput.RawButton.RThumbstick);
+
         
-        // moving
-        // TODO : move dans la direction que pointe le joueur
-        if (_movingAxis != rightAxisTemp)
+        Vector3 playerPos = player.transform.position;
+        Vector3 mapPos = _initialPosition;
+        
+        //Vector3 vectorMap = new Vector3(1, 0, 0);
+        Vector3 vectorPlayer = mapPos - playerPos;
+        vectorPlayer.y = 0;
+        Debug.Log(vectorPlayer);
+
+        if (Math.Abs(rightAxisTemp.x) > 0.5)
         {
-            _movingAxis = rightAxisTemp;
-            _tempPosition = new Vector2(_tempPosition.x + rightAxisTemp.x * movingSpeed * Time.deltaTime,
-                _tempPosition.y + rightAxisTemp.y * movingSpeed * Time.deltaTime);
-            
-            transform.position = new Vector3(_initialPosition.x + _tempPosition.x, _initialPosition.y, _initialPosition.z + _tempPosition.y);
-            Debug.Log("Moving : " + transform.rotation);
+            this.transform.Translate(new Vector3(vectorPlayer.z, vectorPlayer.y, -vectorPlayer.x).normalized * Time.deltaTime * movingSpeed * rightAxisTemp.x);
         }
         
+        if (Math.Abs(rightAxisTemp.y) > 0.5)
+        {
+            this.transform.Translate(vectorPlayer.normalized * Time.deltaTime * movingSpeed * rightAxisTemp.y);
+        }
+
         // reset position
         if (_initialPosition != transform.position & rightButtonThumbstickTemp)
         {
