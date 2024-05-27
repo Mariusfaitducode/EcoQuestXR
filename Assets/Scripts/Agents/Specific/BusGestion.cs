438afee73ca2f519f0e5f1ba721f3ed3631760a9
Victor Goudal <vi.goudal@gmail.com>
2024-05-27 15:54:20 +0200

    rework dashboard

--- Assets/Scripts/Agents/Specific/BusGestion.cs
+++ Assets/Scripts/Agents/Specific/BusGestion.cs
@@ -40,7 +40,7 @@ public class BusGestion : MonoBehaviour
     {
         // Instantiate / Destroy buses with GameManager informations
         
-        Debug.Log("CurrentTime :"+currentTime);
+        // Debug.Log("CurrentTime :"+currentTime);
         
         if (gameManager != null)
         {
@@ -52,7 +52,7 @@ public class BusGestion : MonoBehaviour
         {
             busOut = true;
             
-            Debug.Log("BusGestion Instantiate buses");
+            // Debug.Log("BusGestion Instantiate buses");
             
             // Instantiate buses
             for (int i = 0; i< busQuantity; i++)
