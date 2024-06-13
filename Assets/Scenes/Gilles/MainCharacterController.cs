a58d55c29a270c4bd31d036a992a667e8ebb5f53
Gilles Maurer <gilles.maurer@utbm.fr>
2024-06-13 16:57:09 +0200

    modif audio nuage

--- Assets/Scenes/Gilles/MainCharacterController.cs
+++ Assets/Scenes/Gilles/MainCharacterController.cs
@@ -14,11 +14,14 @@ public class MainCharacterController : MonoBehaviour
     private Color maxValColor = Color.white;
     private Color minValColor = new Color(0.3f, 0.3f, 0.3f);
 
-    private Dictionary<int, string> list_text = new Dictionary<int, string>();
+    private Dictionary<string, string> list_text = new Dictionary<string, string>();
 
     private TextMeshProUGUI text; 
+    private Canvas canva; 
 
     private AudioSource audioSource;
+
+    public List<AudioClip> clipList = new List<AudioClip>();
 
 
     // Start is called before the first frame update
@@ -27,15 +30,21 @@ public class MainCharacterController : MonoBehaviour
 
         character_mat = this.GetComponentInChildren<SkinnedMeshRenderer>().material;
         text = this.GetComponentInChildren<TextMeshProUGUI>();
+        canva = this.GetComponentInChildren<Canvas>();
         audioSource = this.GetComponent<AudioSource>();
 
-        list_text.Add(0, "Bienvenu dans EcoQuest, laisse moi te guider pour tes premiers pas");
-        list_text.Add(1, "Est-ce que tu veux bien m'aider à survivre ?");
-        list_text.Add(2, "SAUVE MOI ET SAUVE LA VILLE");
-        list_text.Add(3, "");
-        list_text.Add(4, "");
+        list_text.Add("play_card3", "Très bien je m'en occupe");
+        list_text.Add("play_card2", "Très bon choix !");
+        list_text.Add("play_card1", "Un pas de plus vers le bonheur !");
+        list_text.Add("random2", "N'hésite pas à aller voir le tutoriel");
+        list_text.Add("random1", "Ca va ? Tu t'en sors ?");
+        list_text.Add("win2", "Bravo ! Grâce à toi la ville est sauvée !");
+        list_text.Add("win1", "Je compte sur toi pour sauver ta vraie planète maintenant !");
+        list_text.Add("lose2", "Oh non, j'ai l'impression que nous n'avons pas réussi à sauver la ville");
+        list_text.Add("lose1", "Je compte sur toi pour rééssayer très bientôt");
 
-        text.text = list_text[0];
+
+        text.text = "";
 
     }
 
@@ -43,6 +52,7 @@ public class MainCharacterController : MonoBehaviour
     void Update()
     {
 
+        canva.enabled = audioSource.isPlaying;
 
     }
 
@@ -60,12 +70,35 @@ public class MainCharacterController : MonoBehaviour
 
 
 
-    public void ChangeText(int new_text_id)
+    public void ChangeText(string new_text_id)
     {
         if (list_text.ContainsKey(new_text_id))
         {
+            canva.enabled = true;
             text.text = list_text[new_text_id];
-            audioSource.Play();
+
+            // Rechercher l'audio clip correspondant
+            AudioClip matchingClip = null;
+            foreach (AudioClip clip in clipList)
+            {
+                if (clip.name == new_text_id)
+                {
+                    matchingClip = clip;
+                    break;
+                }
+            }
+
+            // Si un audio clip correspondant est trouvé, le jouer
+            if (matchingClip != null)
+            {
+                audioSource.clip = matchingClip;
+                audioSource.Play();
+            }
+            else
+            {
+                Debug.LogWarning("Aucun audio clip correspondant trouvé pour " + new_text_id);
+            }
+
         }
     }
 
