e01986064a5261b977acde336a8d525a6a7e02d9
Gilles-H12 <gilles.maurer@utbm.fr>
2024-06-17 21:00:54 +0200

    lundi soir

--- Assets/Scripts/Game/Cards/DisplayCard.cs
+++ Assets/Scripts/Game/Cards/DisplayCard.cs
@@ -17,7 +17,7 @@ public class DisplayCard : MonoBehaviour
     // private PileManager _pileManager;
     // private DisplayCanvas _displayCanvas;
 
-    public TextMeshProUGUI _title;
+    //public TextMeshProUGUI _title;
     public TextMeshProUGUI _description;
     public TextMeshProUGUI _price;
     public RawImage _image;
@@ -26,13 +26,16 @@ public class DisplayCard : MonoBehaviour
     public Button _buttonBackground;
     public RawImage _shadowBackground;
     public RawImage _background;
-    public RawImage _logoArea;
+    public RawImage _zone_background;
+    public RawImage _price_background;
+    public RawImage _text_background;
+    //public RawImage _logoArea;
     public RawImage _logoAction;
 
-    public TextMeshProUGUI _energy;
-    public TextMeshProUGUI _ecology;
-    public TextMeshProUGUI _population;
-    public TextMeshProUGUI _pollution;
+    //public TextMeshProUGUI _energy;
+    //public TextMeshProUGUI _ecology;
+    //public TextMeshProUGUI _population;
+    //public TextMeshProUGUI _pollution;
     
     // Start is called before the first frame update
     
@@ -71,16 +74,47 @@ public class DisplayCard : MonoBehaviour
     {
         if (_card != null)
         {
-            _title.text = _card.title;
+            //_title.text = _card.title;
             _description.text = _card.description;
             
             _image.texture = GetTexture(folderActionImage, _card.cardType.ToString());
-            _background.texture = GetTexture(folderAreaBackground, _card.areaType.ToString());
-            _logoArea.texture = GetTexture(folderAreaLogo, _card.areaType.ToString());
+            //_background.texture = GetTexture(folderAreaBackground, _card.areaType.ToString());
+            //_logoArea.texture = GetTexture(folderAreaLogo, _card.areaType.ToString());
             _logoAction.texture = GetTexture(folderActionLogo, _card.cardType.ToString());
 
+            switch (_card.areaType)
+            {
+                case AreaType.City:
+                    _background.color = new Color(167, 231, 230); // Vert clair
+                    //_zone_background.color = new Color(60, 171, 167); // Vert foncé
+                    //_text_background.color = _background.color; // Couleur du texte identique au fond
+                    //_price_background.color = _zone_background.color; // Couleur du prix identique à la zone
+                    break;
+
+                case AreaType.Industry:
+                    _background.color = new Color(242, 208, 171); // Orange clair
+                    //_zone_background.color = new Color(181, 126, 85); // Orange foncé
+                    //_text_background.color = _background.color;
+                    //_price_background.color = _zone_background.color;
+                    break;
+
+                case AreaType.Energy:
+                    _background.color = new Color(255, 242, 204); // Jaune clair
+                    //_zone_background.color = new Color(208, 171, 115); // Jaune foncé
+                    //_text_background.color = _background.color;
+                    //_price_background.color = _zone_background.color;
+                    break;
+
+                case AreaType.Agriculture:
+                    _background.color = new Color(249, 252, 238); // Vert très clair
+                    //_zone_background.color = new Color(177, 234, 160); // Vert forêt
+                    //_text_background.color = _background.color;
+                    //_price_background.color = _zone_background.color;
+                    break;
+            }
+
             //TODO : Change card stats
-            // _price.text = StatUtils.ConvertNumberToText(_card.cardStats.actionCost);
+            _price.text = _card.cardStats.actionCost.ToString();
             // _energy.text = StatUtils.ConvertNumberToText(_card.cardStats.actionEnergyCost);
             // _ecology.text = StatUtils.ConvertNumberToText(_card.cardStats.ecologyRate);
             // _population.text = StatUtils.ConvertNumberToText(_card.stats.populationSize);
